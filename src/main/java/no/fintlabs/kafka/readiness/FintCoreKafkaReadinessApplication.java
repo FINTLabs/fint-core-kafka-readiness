42d747b1f305603826feadf59c0f1bb525947fdb
Hknots <hknots99@gmail.com>
2024-12-18 15:37:14 +0100

    construct project

---
+++ src/main/java/no/fintlabs/kafka/readiness/FintCoreKafkaReadinessApplication.java
@@ -0,0 +1,13 @@
+package no.fintlabs.kafka.readiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintCoreKafkaReadinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintCoreKafkaReadinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/fintlabs/kafka/readiness/FintCoreKafkaReadinessApplicationTests.java
@@ -0,0 +1,13 @@
+package no.fintlabs.kafka.readiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintCoreKafkaReadinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
